4ddee64918be7cecf4fa859c9991f791e3008d5b
PavelBlessYou51 <prokoshevpavel@mail.ru>
2024-07-12 15:58:17 +0300

    Finish drop tests

--- pages/interections_page.py
+++ pages/interections_page.py
@@ -1,8 +1,10 @@
 import random
+import time
 
 from selenium.webdriver import ActionChains
 
-from locators.interactions_locators import SortablePageLocators, SelectablePageLocators
+from locators.interactions_locators import SortablePageLocators, SelectablePageLocators, ResizablePageLocators, \
+    DroppablePageLocators
 from pages.base_page import BasePage
 
 
@@ -55,3 +57,84 @@ class SelectablePage(BasePage):
         self.click_selectable_item(self.locators.GRID_ITEM)
         active_element = self.element_is_visible(self.locators.GRID_ITEM_ACTIVE)
         return active_element.text
+
+
+class ResizablePage(BasePage):
+    locators = ResizablePageLocators()
+
+    def get_px_from_width_height(self, value_of_size):
+        width = value_of_size.split(';')[0].split(':')[1].replace(' ', '')
+        height = value_of_size.split(';')[1].split(':')[1].replace(' ', '')
+        return width, height
+
+    def get_max_min_size(self, element):
+        size = self.element_is_present(element)
+        size_value = size.get_attribute('style')
+        return size_value
+
+    def change_size_resizable_box(self):
+        handle = self.element_is_present(self.locators.RESIZABLE_BOX_HANDLE)
+        box = self.element_is_present(self.locators.RESIZABLE_BOX)
+        self.go_to_element(handle)
+        self.action_drag_and_drop_by_offset(handle, 400, 200)
+        max_size = self.get_px_from_width_height(self.get_max_min_size(self.locators.RESIZABLE_BOX))
+        self.go_to_element(box)
+        self.action_drag_and_drop_by_offset(handle, -400, -200)
+        min_size = self.get_px_from_width_height(self.get_max_min_size(self.locators.RESIZABLE_BOX))
+        return max_size, min_size
+
+
+class DroppablePage(BasePage):
+    locators = DroppablePageLocators()
+
+    def drop_simple(self):
+        drag_div = self.element_is_visible(self.locators.DRAG_ME_SIMPLE)
+        drop_div = self.element_is_visible(self.locators.DROP_HERE_SIMPLE)
+        self.go_to_element(drag_div)
+        self.action_drag_and_drop_to_element(drag_div, drop_div)
+        return drop_div.text
+
+    def drop_accept(self):
+        self.element_is_visible(self.locators.ACCEPT_TAB).click()
+        acceptable_div = self.element_is_visible(self.locators.ACCEPTABLE)
+        self.go_to_element(acceptable_div)
+        not_acceptable_div = self.element_is_visible(self.locators.NOT_ACCEPTABLE)
+        drop_div = self.element_is_visible(self.locators.DROP_HERE_ACCEPT)
+        self.action_drag_and_drop_to_element(not_acceptable_div, drop_div)
+        drop_text_not_accept = drop_div.text
+        self.action_drag_and_drop_to_element(acceptable_div, drop_div)
+        drop_text_accept = drop_div.text
+        return drop_text_not_accept, drop_text_accept
+
+    def drop_prevent_propogation(self):
+        self.element_is_visible(self.locators.PREVENT_TAB).click()
+        drag_div = self.element_is_visible(self.locators.DRAG_ME_PREVENT)
+        not_greedy_inner_box = self.element_is_visible(self.locators.NOT_GREEDY_INNER_BOX)
+        greedy_inner_box = self.element_is_visible(self.locators.GREEDY_INNER_BOX)
+        self.action_drag_and_drop_to_element(drag_div, not_greedy_inner_box)
+        self.go_to_element(drag_div)
+        text_not_greedy_box = self.element_is_visible(self.locators.NOT_GREEDY_DROP_BOX_TEXT).text
+        text_not_greedy_inner_box = not_greedy_inner_box.text
+        self.action_drag_and_drop_to_element(drag_div, greedy_inner_box)
+        text_greedy_box = self.element_is_visible(self.locators.GREEDY_DROP_BOX_TEXT).text
+        text_greedy_inner_box = greedy_inner_box.text
+        return text_not_greedy_box, text_not_greedy_inner_box, text_greedy_box, text_greedy_inner_box
+
+    def drop_revert_draggable(self, type_drag):
+        drags = {
+            'will':
+                {'revert': self.locators.WILL_REVERT, },
+            'not_will':
+                {'revert': self.locators.NOT_REVERT},
+        }
+        self.element_is_visible(self.locators.REVERT_TAB).click()
+        revert = self.element_is_visible(drags[type_drag]['revert'])
+        drop_div = self.element_is_visible(self.locators.DROP_HERE_REVERT)
+        self.go_to_element(drop_div)
+        self.action_drag_and_drop_to_element(revert, drop_div)
+        position_after_move = revert.get_attribute('style')
+        time.sleep(1)
+        position_after_revert = revert.get_attribute('style')
+        return position_after_move, position_after_revert
+
+
--- tests/interections_tests.py
+++ tests/interections_tests.py
@@ -1,4 +1,4 @@
-from pages.interections_page import SortablePage, SelectablePage
+from pages.interections_page import SortablePage, SelectablePage, ResizablePage, DroppablePage
 
 
 class TestInteractions:
@@ -19,3 +19,41 @@ class TestInteractions:
             item_grid = selectable_page.select_grid_item()
             assert len(item_list) > 0, "No elements were selected"
             assert len(item_grid) > 0, "No elements were selected"
+
+    class TestResizablePage:
+        def test_resizable(self, driver):
+            resizable_page = ResizablePage(driver, 'https://demoqa.com/resizable')
+            resizable_page.open()
+            max_box, min_box = resizable_page.change_size_resizable_box()
+            assert max_box == ('500px', '300px'), "Problem with max size"
+            assert min_box == ('150px', '150px'), "Problem with min size"
+
+    class TestDropablePage:
+        def test_simple_dropable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            text = droppable_page.drop_simple()
+            assert text == 'Dropped!', "Simple element has not been dropped"
+
+        def test_accert_dropable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            not_accept, accept = droppable_page.drop_accept()
+            assert not_accept != accept, 'The element one not has been dropped'
+
+        def test_prevent_propogation_dropable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            not_greedy, not_greedy_inner, greedy, greedy_inner = droppable_page.drop_prevent_propogation()
+            assert not_greedy == 'Dropped!', "The element text has not been changed"
+            assert not_greedy_inner == 'Dropped!', "The element text has not been changed"
+            assert greedy == 'Outer droppable', "The element text has been changed"
+            assert greedy_inner == 'Dropped!', "The element text has not been changed"
+
+        def test_revert_draggable_dropable(self, driver):
+            droppable_page = DroppablePage(driver, 'https://demoqa.com/droppable')
+            droppable_page.open()
+            will_after_move, will_after_revert = droppable_page.drop_revert_draggable('will')
+            not_will_after_move, not_will_after_revert = droppable_page.drop_revert_draggable('not_will')
+            assert will_after_move != will_after_revert, "Didn't revert"
+            assert not_will_after_move == not_will_after_revert, "Revert"
